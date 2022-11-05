1b6042f07f82d0b7ec270e27cd3832120ecf6ca5
cmd <cmd@yggdrasil.onion>
2022-11-04 23:50:30 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import * as secp from '@noble/secp256k1'
-import Cipher from './cipher.js'
-import * as Hash from './hash.js'
+import Cipher     from './cipher.js'
+import * as Hash  from './hash.js'
+import * as Keys  from './keys.js'
+import * as Rand  from './rand'
+import * as Utils from './utils.js'
 
 export default {
   Cipher,
   Hash,
-  secp
+  Keys,
+  Rand,
+  Utils
 }
