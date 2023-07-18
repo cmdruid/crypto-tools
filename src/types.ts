442a424bc949c724b037b009ca5d1f2282a1bcee
cmd <cmd@yggdrasil.onion>
2023-07-18 12:58:45 -0500

    update

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,4 @@
-import { Buff }   from '@cmdcode/buff-utils'
-import { Signer } from './signer.js'
+import { Buff } from '@cmdcode/buff-utils'
 
 export type HashTypes  = 'sha256'  | 'sha512' | 'hash256' | 'ripe160' | 'hash160'
 export type HmacTypes  = 'hmac256' | 'hmac512'
@@ -7,8 +6,6 @@ export type HmacTypes  = 'hmac256' | 'hmac512'
 export type HDKey = [ key : Buff, code : Buff ]
 
 export interface PointData { x : bigint, y : bigint }
-
-export interface SignerAPI extends Signer {}
 
 export interface ExtendedKey {
   prefix : number
