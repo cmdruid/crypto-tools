23075b151a2a18fb4fb757b3c6bc3ec7013da929
cmd <cmd@yggdrasil.onion>
2024-01-09 20:10:07 -0600

    updated ecdh again

---
+++ src/lib/ecdh.ts
@@ -0,0 +1,33 @@
+import { Buff, Bytes } from '@cmdcode/buff'
+import { Point }       from './ecc.js'
+import { hmac512 }     from './hash.js'
+
+import {
+  get_pubkey,
+  get_seckey,
+  parse_pubkey
+}  from './keys.js'
+
+export function ecdh (
+  self_sec : Bytes,
+  peer_pub : Bytes,
+  xonly    = false
+) : Buff {
+  const P   = Point.from_x(peer_pub)
+  const sec = get_seckey(self_sec)
+  const shared = P.mul(sec)
+  return (xonly) ? shared.x : shared.buff
+}
+
+export function ecdhash (
+  message  : Bytes,
+  self_sec : Bytes,
+  peer_pub : Bytes,
+  xonly    = false
+) : Buff {
+  const p1   = get_pubkey(self_sec, xonly)
+  const p2   = parse_pubkey(peer_pub, xonly)
+  const seed = ecdh(self_sec, peer_pub, xonly)
+  const pubs = [ p1.hex, p2.hex ].sort()
+  return hmac512(seed, ...pubs, message)
+}
